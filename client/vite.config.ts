b1306880d94c5707f4471ab58ecffc1ff72c2da3
FlareVM Admin <admin@flarevm.com>
2026-02-20 19:42:33 +0530

    Fix: Explicitly register Tailwind in Vite config for maximum compatibility

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss('./tailwind.config.js'),
+        autoprefixer(),
+      ],
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173
